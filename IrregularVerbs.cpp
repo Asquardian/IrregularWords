342c319fc6d8c458ac5d27e8e00300ea0963f52b
Asquardian <alexromanov5262@gmail.com>
2020-04-16 21:58:42 +0700

    Made working functional

---
+++ IrregularVerbs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+ 
+using namespace std;
+ 
+int main()
+{
+	string str, word;
+	cout << "Enter the word: ";
+	cin >> str;
+	word = "Was";
+	if(str == word)
+	cout << "plus 1";
+}
