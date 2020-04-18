8f9fd253e55980f6cfc3797ba3b0ca5252e4fa85
Alexey <alexromanov5262@gmail.com>
2020-04-18 16:18:19 +0700

    Added New Word

--- Words.cpp
+++ Words.cpp
@@ -6,13 +6,29 @@ string Words(int i)
     case 0:
         cout << "Simple Past (V2) of Arise" << endl;
         Checker("Arose");
+	break;
     case 1:
         cout << "Past Particle (V3) of Arise" << endl;
         Checker("Arisen");
+	break;
     case 2:
         cout << "Simple Past (V2) of Awake without -ed" << endl;
         Checker("Awoke");
+	break;
     case 3:
         cout << "Past Particle (V3) of Awake without -ed" << endl;
         Checker("Awoken");
+	break;
+    case 4:
+        cout << "Simple Past (V2) of Be in singular" << endl;
+        Checker("Was");
+	break;
+    case 5:
+        cout << "Simple Past (V2) of Be in plural" << endl;
+        Checker("Were");
+	break;
+    case 6:
+        cout << "Past Particle (V3) of Be" << endl;
+        Checker("Been");
+	break;
     }
