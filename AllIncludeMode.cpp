f05f09c8cd43dd9197d90fc249dd725496de38f5
Alexey <alexromanov5262@gmail.com>
2020-04-21 11:48:33 +0700

    Changed Menu

--- AllIncludeMode.cpp
+++ AllIncludeMode.cpp
@@ -3,7 +3,6 @@
 void Mode1()
 {
     int i;
-        cout << "Enter word from a capital letter";
         for (i = 0; i < 100; i++) {
             Words(i);
         }
--- IrregularVerbs.cpp
+++ IrregularVerbs.cpp
@@ -2,5 +2,8 @@
 
 int main()
 {	
-	Mode1();
+	cout << endl << "            Welcome to Irregular Verbs Checker Beta" << endl;
+	cout << endl << "            Input your words from a capital letter" << endl;
+	cout << endl << "Input 'Exit' to go in main menu when application will be started" << endl;
+	Selector();
 }
