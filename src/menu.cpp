a0c880564fec7e5fcc55671c7f8413ff0d4924aa
Alexey <alexromanov5262@gmail.com>
2020-04-24 19:08:08 +0700

    Add ctest.h

--- src/menu.cpp
+++ src/menu.cpp
@@ -4,6 +4,8 @@ int Menu()
 {
     int n = 0;
     system("clear");
+    cout << "        Enter 'Exit' or 'exit' to quit" << endl;
+    cout << "  Enter 'ExitMenu' or 'exitmenu' to quit in menu" << endl;
     while (n == 0) {
         system("figlet VerbsChecker");
         cout << "                  Enter Mode: All or Random" << endl;
