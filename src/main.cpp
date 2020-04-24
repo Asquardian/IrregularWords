a26558c1b41cc127803968b455cc2f6055b72bc5
Alexey <alexromanov5262@gmail.com>
2020-04-24 18:21:36 +0700

    Create menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include "functions.h"
 
 int main()
-{	
-	Mode2();
-	return 0;
+{
+    int n = 0;
+    while (n == 0) {
+        system("figlet VerbsChecker");
+        cout << "                  Enter Mode: All or Random" << endl;
+        string Choose;
+        cin >> Choose;
+        if (Choose == "All" || Choose == "all") {
+            Mode1();
+        }
+        if (Choose == "Random" || Choose == "random") {
+            Mode2();
+        } else
+            cout << "Error";
+    }
+    return 0;
 }
