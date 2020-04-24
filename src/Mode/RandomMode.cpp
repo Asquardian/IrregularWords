4a746cbb89796553a60e1094ea845b3882f0721f
Alexey <alexromanov5262@gmail.com>
2020-04-24 17:50:29 +0700

    Write new function

---
+++ src/Mode/RandomMode.cpp
@@ -0,0 +1,23 @@
+#include "../functions.h"
+
+int Mode2()
+{
+    int i, z = 0, n = 0;
+    while (n < 12.5) {
+        i = rand() % 351;
+        while (i % 3 != 0) {
+            i++;
+        }
+        while (z < 3) {
+            if (i % 3 == 0) {
+                cout << "Word is ";
+            }
+            Words(i);
+            i++;
+            z++;
+        }
+	n++;
+	z = 0;
+    }
+    return 0;
+}
