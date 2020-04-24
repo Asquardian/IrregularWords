8ab74e3b2eff4f2e45fff00e153fe6b68b2a1cb6
Alexey <alexromanov5262@gmail.com>
2020-04-24 18:54:49 +0700

    Added clear terminal in Mode2

--- src/Mode/RandomMode.cpp
+++ src/Mode/RandomMode.cpp
@@ -10,6 +10,8 @@ int Mode2()
         }
         while (z < 3) {
             if (i % 3 == 0) {
+		sleep(1);
+		system("clear");
                 cout << "Word is ";
             }
             Words(i);
