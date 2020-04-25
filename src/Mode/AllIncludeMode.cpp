14284ddec7c4884c3f941a4934cc23dc623b79e0
Alexey <alexromanov5262@gmail.com>
2020-04-25 20:25:20 +0700

    Make code style

--- src/Mode/AllIncludeMode.cpp
+++ src/Mode/AllIncludeMode.cpp
@@ -1,19 +1,20 @@
 #include "../functions.h"
 
-int Mode1() {
-  for (int i = 3; i < 353.5; i++) {
-      if (i % 3 == 0) {
-          sleep(1);
-	  system("clear");
-          cout << "Word is ";
-      }
-      if (i % 3 == 2)
-          cout << "PP V3 ";	
-      if (i % 3 == 1)
-	  cout << "PS V2 ";
-      Words(i);
-      if (i == 3)
-          cout << "Enter like Singular/Plural" << endl;
-      }
-  return 0;
+int Mode1()
+{
+    for (int i = 3; i < 353.5; i++) {
+        if (i % 3 == 0) {
+            sleep(1);
+            system("clear");
+            cout << "Word is ";
+        }
+        if (i % 3 == 2)
+            cout << "PP V3 ";
+        if (i % 3 == 1)
+            cout << "PS V2 ";
+        Words(i);
+        if (i == 3)
+            cout << "Enter like Singular/Plural" << endl;
+    }
+    return 0;
 }
--- src/Mode/RandomMode.cpp
+++ src/Mode/RandomMode.cpp
@@ -1,24 +1,25 @@
 #include "../functions.h"
 
-int Mode2() {
-  int i, z = 0, n = 0;
-  while (n < 12.5) {
-      i = rand() % 351;
-      while (i % 3 != 0) {
-          i++;
-      }
-      while (z < 3) {
-          if (i % 3 == 0) {
-              sleep(1);
-	      system("clear");
-              cout << "Word is ";
-          }
-          Words(i);
-          i++;
-          z++;
-      }
-      n++;
-      z = 0;
-  }
-  return 0;
+int Mode2()
+{
+    int i, z = 0, n = 0;
+    while (n < 12.5) {
+        i = rand() % 351;
+        while (i % 3 != 0) {
+            i++;
+        }
+        while (z < 3) {
+            if (i % 3 == 0) {
+                sleep(1);
+                system("clear");
+                cout << "Word is ";
+            }
+            Words(i);
+            i++;
+            z++;
+        }
+        n++;
+        z = 0;
+    }
+    return 0;
 }
