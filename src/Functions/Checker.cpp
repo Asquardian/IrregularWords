d9eb3d85f08b0f834d11a723a3d778409eee5141
Alexey <alexromanov5262@gmail.com>
2020-04-26 16:27:55 +0700

    Make code style

--- src/Functions/Checker.cpp
+++ src/Functions/Checker.cpp
@@ -8,19 +8,19 @@ void Checker(string OUTPUT)
     cout << "Enter a word: ";
     cin >> INPUT;
     if (INPUT == "Exit" || INPUT == "exit") {
-	OUT.open("stat.forge");
-	OUT << 0;
+        OUT.open("stat.forge");
+        OUT << 0;
         exit(0);
-	}
+    }
     if (INPUT == "ExitMenu" || INPUT == "exitmenu")
         Menu();
     if (INPUT == OUTPUT) {
         cout << "Correct " << endl;
-	IN.open("stat.forge"); 
-	IN >> j;
-	j++;
-	OUT.open("stat.forge");
-	OUT << j;
+        IN.open("stat.forge");
+        IN >> j;
+        j++;
+        OUT.open("stat.forge");
+        OUT << j;
     } else
         cout << "You made a mistake" << endl;
 }
