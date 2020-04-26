4e4f17b93a58cbcb2d8ee0ad806cd6fc939016a6
Alexey <alexromanov5262@gmail.com>
2020-04-26 15:16:38 +0700

    Add save for 1 person

--- src/menu.cpp
+++ src/menu.cpp
@@ -7,6 +7,8 @@ int Menu()
     system("clear");
     cout << "        Enter 'Exit' or 'exit' to quit" << endl;
     cout << "  Enter 'ExitMenu' or 'exitmenu' to quit in menu" << endl;
+    cout << "  Enter 'Save' or 'save' to save your progress" << endl;
+    cout << "  Enter 'Progress' or 'progress' to see your progress" << endl;
     while (n == 0) {
         system("figlet VerbsChecker");
         cout << "                  Enter Mode: All or Random" << endl;
@@ -18,23 +20,30 @@ int Menu()
         if (Choose == "Random" || Choose == "random") {
             Mode2();
         }
-	if (Choose == "Save" || Choose == "save") {
-	    cout << "Enter your name: ";
-            cin >> a;
-	    cout << a;
-	    ifstream outs;
-            ofstream s;
-	    cout << " Saving... ";
-	    outs.open("stat.forge");
-	    outs >> j;
-	    s.open("score.forge");
-            s << a << ": " <<  j << "\n";
-	    cout << "Saving complete";
-	}
+        if (Choose == "Save" || Choose == "save") {
+            cout << "Do you want to rewrite score? y/n";
+            string answ;
+            cin >> answ;
+            if (answ == "y") {
+                cout << "Enter your name: ";
+                cin >> a;
+                cout << a;
+                ifstream outs, pck;
+                ofstream save;
+                cout << " Saving... ";
+                outs.open("stat.forge");
+                outs >> j;
+                outs.close();
+                save.open("score.forge");
+                save << a << ": " << j;
+                save.close();
+                cout << "Saving complete";
+            }
+        }
         if (Choose == "Exit" || Choose == "exit") {
             exit(0);
-	}
-        if (Choose != "Save" && Choose != "save")
+        }
+        if (Choose != "Save" && Choose != "save" && Choose != "Progress" && Choose != "progress")
             cout << " Error";
         system("clear");
     }
