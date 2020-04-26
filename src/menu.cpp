ae86102ad9e1093c47e4c8891808d6f6a1a46708
Alexey <alexromanov5262@gmail.com>
2020-04-26 16:19:46 +0700

    Add check progress

--- src/menu.cpp
+++ src/menu.cpp
@@ -7,11 +7,11 @@ int Menu()
     system("clear");
     cout << "        Enter 'Exit' or 'exit' to quit" << endl;
     cout << "  Enter 'ExitMenu' or 'exitmenu' to quit in menu" << endl;
-    cout << "  Enter 'Save' or 'save' to save your progress" << endl;
+    cout << "  Enter 'Save' or 'save' to save your progress" << endl; 
     cout << "  Enter 'Progress' or 'progress' to see your progress" << endl;
+    system("figlet VerbsChecker");
+    cout << "                  Enter Mode: All or Random" << endl;
     while (n == 0) {
-        system("figlet VerbsChecker");
-        cout << "                  Enter Mode: All or Random" << endl;
         string Choose;
         cin >> Choose;
         if (Choose == "All" || Choose == "all") {
@@ -21,31 +21,40 @@ int Menu()
             Mode2();
         }
         if (Choose == "Save" || Choose == "save") {
-            cout << "Do you want to rewrite score? y/n";
+            cout << "Do you want to rewrite score? y/n" << endl;
             string answ;
             cin >> answ;
             if (answ == "y") {
                 cout << "Enter your name: ";
                 cin >> a;
                 cout << a;
-                ifstream outs, pck;
+                ifstream outs;
                 ofstream save;
-                cout << " Saving... ";
+                cout << " Saving... " << endl; 
                 outs.open("stat.forge");
-                outs >> j;
+                outs >> j; 
                 outs.close();
                 save.open("score.forge");
-                save << a << ": " << j;
+                save << a << ":" << j;
                 save.close();
-                cout << "Saving complete";
+                cout << "Saving complete" << endl; 
             }
         }
+	if (Choose == "Progress" || Choose == "progress") {
+	    ifstream check;
+            string a;
+	    check.open("score.forge");
+            check >> a;
+	    cout << a << endl;
+            }
         if (Choose == "Exit" || Choose == "exit") {
             exit(0);
         }
-        if (Choose != "Save" && Choose != "save" && Choose != "Progress" && Choose != "progress")
-            cout << " Error";
-        system("clear");
+        if (Choose != "Save" && Choose != "save" && Choose != "Progress" && Choose != "progress") {
+            system("clear");
+	    system("figlet VerbsChecker");
+    	    cout << "                  Enter Mode: All or Random" << endl;
+	}
     }
     return 0;
 }
