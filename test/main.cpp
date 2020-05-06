05ff381835fc4b2a0c4d4c418ae7e78fea3a0b11
Alexey <alexromanov5262@gmail.com>
2020-05-06 09:50:07 +0700

    Write new test

--- test/main.cpp
+++ test/main.cpp
@@ -1,21 +1,39 @@
-#include <gtest/gtest.h>
 #include <cstring>
+#include <gtest/gtest.h>
 
 using namespace std;
 
-TEST(StringTest, SpaceTest)
+TEST(StringTest, commentTest)
 {
     // Given
-    char comment_string[] = "Abc";
-    char comment_string_1[] = "123";
+    char comment_string[] = " //";
+    char comment_string_1[] = " ///";
     // When
     string str1 = comment_string;
     string str2 = comment_string_1;
-    string str1origin = "Abc";
-    string str2origin = "123";
+    string str1origin = " //";
+    string str2origin = " ///";
     // Then
     EXPECT_EQ(str1, str1origin);
     EXPECT_EQ(str2, str2origin);
+}
+TEST(StringTest, CommentStarTest)
+{
+    // Given
+    char comment_string[] = " */";
+    char comment_string_1[] = " /*";
+    char comment_string_2[] = " /* */";
+    // When
+    string str1 = comment_string;
+    string str2 = comment_string_1;
+    string str3 = comment_string_2;
+    string str1origin = " */";
+    string str2origin = " /*";
+    string str3origin = " /* */";
+    // Then
+    EXPECT_EQ(str1, str1origin);
+    EXPECT_EQ(str2, str2origin);
+    EXPECT_EQ(str3, str3origin);
 }
 
 int main(int argc, char** argv)
