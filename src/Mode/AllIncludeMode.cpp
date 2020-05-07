a05ab7242ce04ae4d2482e6b395978e6dbf36add
Alexey <alexromanov5262@gmail.com>
2020-05-07 12:38:38 +0700

    Add checkfile

--- src/Mode/AllIncludeMode.cpp
+++ src/Mode/AllIncludeMode.cpp
@@ -2,22 +2,25 @@
 
 int Mode1(int i)
 {
-    while (i < 353.5) {
-        if (i % 3 == 0) {
-            sleep(1);
-            system("clear");
-            cout << "Word is ";
+    if (i >= 3) {
+        while (i < 353.5) {
+            if (i % 3 == 0) {
+                sleep(1);
+                system("clear");
+                cout << "Word is ";
+            }
+            if (i % 3 == 2)
+                cout << "PP V3 ";
+            if (i % 3 == 1)
+                cout << "PS V2 ";
+            Words(i);
+            if (i == 3)
+                cout << "Enter like Singular/Plural" << endl;
+            i++;
+            ofstream cont("continue.forge");
+            cont << i;
         }
-        if (i % 3 == 2)
-            cout << "PP V3 ";
-        if (i % 3 == 1)
-            cout << "PS V2 ";
-        Words(i);
-        if (i == 3)
-            cout << "Enter like Singular/Plural" << endl;
-        i++;
-	ofstream cont("continue.forge");
-	cont << i;
+        return 1;
     }
     return 0;
 }
