2b87bf23b85b7e649df90d9090757705d85190be
Alexey <alexromanov5262@gmail.com>
2020-05-29 15:32:26 +0700

    Make new test

--- test/testall.cpp
+++ test/testall.cpp
@@ -6,10 +6,15 @@ using namespace std;
 
 TEST(WordsTest, MinTest) { EXPECT_EQ(Words(3), "Be"); }
 TEST(WordsTest, MaxTest) { EXPECT_EQ(Words(353), "Written"); }
-TEST(WordsTest, OutRangeTest) { EXPECT_EQ(Words(354), ""); }
-TEST(Save, Success) {
-  EXPECT_EQ(Save("Name"), "SaveSuccess");
+TEST(WordsTest, TimeTest) {
+  int r = 3;
+  while (r < 353) {
+    Words(r);
+    r++;
+  }
 }
+TEST(WordsTest, OutRangeTest) { EXPECT_EQ(Words(354), ""); }
+TEST(Save, Success) { EXPECT_EQ(Save("Name"), "SaveSuccess"); }
 TEST(ModeTest, Mintest) { EXPECT_EQ(All(0), 0); }
 TEST(ModeTest, OutRange) {
   EXPECT_EQ(All(354), 0); // 353 -number of words
