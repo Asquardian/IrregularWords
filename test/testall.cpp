6a4f1246bf1fbdbcc340c4f8ecc8d32309543609
Alexey <alexromanov5262@gmail.com>
2020-05-29 15:27:40 +0700

    Fix test

--- test/testall.cpp
+++ test/testall.cpp
@@ -8,11 +8,8 @@ TEST(WordsTest, MinTest) { EXPECT_EQ(Words(3), "Be"); }
 TEST(WordsTest, MaxTest) { EXPECT_EQ(Words(353), "Written"); }
 TEST(WordsTest, OutRangeTest) { EXPECT_EQ(Words(354), ""); }
 TEST(Save, Success) {
-  EXPECT_EQ(Save(1, "Name"), "SaveSuccess");
-  EXPECT_EQ(Save(1, "Name"), "SaveSuccess");
+  EXPECT_EQ(Save("Name"), "SaveSuccess");
 }
-TEST(Save, Error) { EXPECT_EQ(Save(0, "Name"), "0"); }
-TEST(Save, max) { EXPECT_EQ(Save(9, "Name"), "SaveSuccess"); }
 TEST(ModeTest, Mintest) { EXPECT_EQ(All(0), 0); }
 TEST(ModeTest, OutRange) {
   EXPECT_EQ(All(354), 0); // 353 -number of words
